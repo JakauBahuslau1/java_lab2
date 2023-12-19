35c955dbaba426cd9a67d26acd54be20ab44a77f
JakauBahuslau1 <rct.leonenko@bsu.by>
2023-12-19 12:07:57 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package bsu.rfe.java.group8.lab2.Leonenko.varA11;
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.sql.SQLOutput;
+
+//все правильно
+public class Main {
+    public static void main(String[] args) {
+	    MainFrame frame=new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
